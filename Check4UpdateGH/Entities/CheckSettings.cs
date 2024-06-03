54e8152e1c9737f14cf6ce1f013a70ff2afa1c42
Alex Virlan <dj.3agl3@gmail.com>
2024-06-03 06:32:08 +0300

    Adding CheckSettings class

---
+++ Check4UpdateGH/Entities/CheckSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Check4UpdateGH.Entities.Enums;
+
+namespace Check4UpdateGH.Entities
+{
+    public class CheckSettings
+    {
+        #region Properties
+        public CheckType CheckType { get; set; }
+        public CheckVersionFrom CheckVersionFrom { get; set; }
+        #endregion
+
+        #region Constructors
+        public CheckSettings()
+        {
+            CheckType = CheckType.VersionThenTime;
+            CheckVersionFrom = CheckVersionFrom.TagName;
+        }
+        #endregion
+    }
+}
